5ae12e2bcd38ef972c9736d7ea750f7e76ccbca2
Steve George Varghese <stevegv2000@gmail.com>
2021-06-05 19:26:43 +0400

    Assignment-03 submission for Wallis and Monte Carlo method for approximating pi

--- monte_carlo.c
+++ monte_carlo.c
@@ -11,6 +11,26 @@ float frandom() {
   return ret;
 }
 
+float mc_pi(int n) {
+	float circle_points = 0;
+	float square_points = 0;
+	for(int i = 1; i<= n; i++) {
+		float x = frandom();
+		float y = frandom();
+		if ((pow(x,2)+pow(y,2)) <= 1.0) {
+			circle_points += 1;
+		}
+		else {
+			square_points += 1;
+		}
+	}
+	//printf("%f\n", circle_points);
+	//printf("%f\n", square_points);
+	square_points += circle_points;
+	float pi = 4.0*(float)circle_points/square_points;
+	return pi;
+}
+	
 int main(void) {
   float pi0;
   float pi1;
---
+++ wallis_copy.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+float wallis_pi(int n)
+{
+        float m = n;
+        float pi =1;
+        float eq;
+        for(float i=1;i<=m;i++)
+        {
+                eq = (4*i*i)/((4*i*i)-1);
+                pi *= eq;
+        }
+        pi *=2;
+        return pi;
+}
+
+int main(void) {
+  float pi;
+  for (int i=0; i<5; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) > 0.15)) {
+      printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
+      abort();
+    }
+  }
+
+  for (int i=500; i<3000; i++) {
+    pi = wallis_pi(i);
+    if (!(fabs(pi - M_PI) < 0.01)) {
+      printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
+      abort();
+    }
+  }
+}
